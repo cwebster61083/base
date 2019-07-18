120f3d25d024c625da1714521406643825812cd3
Chris Webster <chirs.webster@puppet.com>
2019-07-18 16:08:32 -0700

    stuf

--- manifests/template.pp
+++ manifests/template.pp
@@ -4,14 +4,12 @@
 #
 # @example
 #   include base::template
-class base::template {(
-  Sensitive[String] $sensitive_bindDNpassword,
-)
+class base::template {
   file { '/tmp/test_file.txt':
       ensure  => 'file',
       owner   => 'root',
       group   => 'root',
       mode    => '0644',
-      content => epp('test.epp',{'bindDNpassword'=>$sensitive_bindDNpassword}),
+      content => epp('test.epp'),
     }
 }
